fd70bb2fab7cf0ec6b54b558be2857c855ebb4cf
VladislaVik <vlad16396@gmail.com>
2021-10-27 11:26:19 +0300

    second lesson

--- 1_2.py
+++ 1_2.py
@@ -1,12 +1,6 @@
-# 2. Пользователь вводит время в секундах.
-# Переведите время в часы, минуты и секунды и выведите в формате чч:мм:сс.
-# Используйте форматирование строк.
+my_list = input("Напишите числа через пробел: ").split()
 
-time = int(input('введите секунды:'))
-houre = time // 3600
-minetes = (time - houre * 3600) // 60
-seconds = time - 60 * minetes - houre * 3600
-#print('часы:', houre)
-#print('минуты:', minetes)
-#print('секунды:', seconds)
-print('Время в ч:м:с =',houre,':',minetes,':',seconds)
+for i in range(1, len(my_list), 2):
+    my_list.insert(i - 1, my_list.pop(i))
+
+print (my_list)
