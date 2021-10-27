9d1cb8bedb14a47847ce72994e7bd1b8385b9534
VladislaVik <vlad16396@gmail.com>
2021-10-27 11:14:24 +0300

    second lesson

--- 1_1.py
+++ 1_1.py
@@ -1,10 +1,7 @@
-#1. Поработайте с переменными, создайте несколько, выведите на экран,
-# запросите у пользователя несколько чисел и строк и сохраните в переменные, выведите на экран.
-a = "Hi"
-b = "country"
-print(f"{a}, {b}")
-number1 = int(input('введите первое число:'))
-number2 = int(input('введите второе число:'))
-print(f"Вы указали числа {number1} and {number2}")
-word = input("Введите любое слово: ")
-print(f"{word} - Мне нравиться Ваш выбор")
+my_list = [(-5 + 0j)], 2, 3.4, None, True, \
+          [5, 4], (4, 3, 4.3), {7: 'seven', 8: 'eight'}, \
+          frozenset(), range(11), b'twelwe', bytearray(b'thirteen'), \
+          zip(*[(17, 18), (19, 20), ('a', 'b')])
+
+for i, item in enumerate(my_list, 1):
+    print(f"{i}) {item} - {type(item)}")
